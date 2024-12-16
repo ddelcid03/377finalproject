75d5e972cfff418c92dba10015cb0dcef3b56739
Diego Delcid <diegosdelcid@gmail.com>
2024-12-15 22:40:40 -0500

    code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('./api');
 const app = express();
 
 app.use(cors());
